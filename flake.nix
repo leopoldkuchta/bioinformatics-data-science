d61bd2f2a6051b057cb6ba786bc2821b5615d36c
Leopold Kuchta <leopold.kuchta@gmail.com>
2026-04-19 15:43:11 +0200

    Add WIP marker to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,28 +1,3 @@
 {
-  description = "Bioinformatics-Datascience env";
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-  outputs =
-    { flake-utils, nixpkgs, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { 
-          inherit system; 
-          config.allowUnfree = true;
-        };
-        fhs = pkgs.buildFHSEnv {
-          name = "pixi-positron-env";
-
-          targetPkgs = _: [ 
-            pkgs.pixi 
-            pkgs.positron-bin
-          ];
-        };
-      in
-      {
-        devShell = fhs.env;
-      }
-    );
+WIP
 }
